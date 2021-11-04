9d2844314a9b173a913abfb95a2e46622edd0e06
denny0754 <me@dvaccher.com>
2021-11-04 09:40:46 +0100

    upd: version test fix: curl requests utility
    Author: Diego Vaccher <me@dvaccher.com>

--- test/pve-root-tests/version.test.js
+++ test/pve-root-tests/version.test.js
@@ -11,9 +11,7 @@ describe('root.version', () => {
     it('Version', async () => {
         pveapi.SetSslVerifyHost(false);
         pveapi.SetSslVerifyPeer(false);
-        var response = await pveapi.Version.Post();
-        console.log(response);
-        console.log(response.header)
+        var response = await pveapi.Version.Get();
         assert.equal(response.statusCode, 200);
     }).timeout(10000);
 });
