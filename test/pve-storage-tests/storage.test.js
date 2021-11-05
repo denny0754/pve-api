0917f21672f45b4e0cc53f867db6e37a26bc18cb
denny0754 <me@dvaccher.com>
2021-11-05 09:19:39 +0100

    upd: remove unnecessary tests
    Author: Diego Vaccher <me@dvaccher.com>

--- test/pve-storage-tests/storage.test.js
+++ test/pve-storage-tests/storage.test.js
@@ -39,36 +39,3 @@ describe('storage.get', () => {
 
     }).timeout(10000);
 });
-
-describe('storage.get', () => {
-    it('ALL_IN_ONE_200_OK', async () => {
-        const pveapi = RequireUnCached('../../lib/pve-api')(
-            process.env.UNAME,
-            process.env.PASSWORD,
-            process.env.REALM || 'pam',
-            process.env.HOST_NAME || process.env.HOST_IPV4,
-            process.env.VERBOSE_LEVEL || -1
-        );
-
-        pveapi.SetSslVerifyHost(false);
-        pveapi.SetSslVerifyPeer(false);
-        var response = await pveapi.Storage.Get("");
-        assert.equal(response.statusCode, 200);
-    }).timeout(10000);
-
-    it('STANDALONE_200_OK', async () => {
-        const pve_storage_api = RequireUnCached('../../lib/pve-storage-api')(
-            process.env.UNAME,
-            process.env.PASSWORD,
-            process.env.REALM || 'pam',
-            process.env.HOST_NAME || process.env.HOST_IPV4,
-            process.env.VERBOSE_LEVEL || -1
-        );
-
-        pve_storage_api.SetSslVerifyHost(false);
-        pve_storage_api.SetSslVerifyPeer(false);
-        var response = await pve_storage_api.Get("");
-        assert.equal(response.statusCode, 200);
-
-    }).timeout(10000);
-});
