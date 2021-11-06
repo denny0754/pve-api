b57b0557bb3c17b8e44777a5de1b8cd903e7d053
denny0754 <me@dvaccher.com>
2021-11-06 11:34:16 +0100

    add: nodes/:node/apt* route tests(GET ONLY)
    Author: Diego Vaccher <me@dvaccher.com>

--- test/pve-nodes-tests/nodes.test.js
+++ test/pve-nodes-tests/nodes.test.js
@@ -1,6 +1,14 @@
 const assert = require('assert');
 require('dotenv').config();
 
+var envTestParams = [
+    process.env.UNAME,
+    process.env.PASSWORD,
+    process.env.REALM || 'pam',
+    process.env.HOST_NAME || process.env.HOST_IPV4,
+    process.env.VERBOSE_LEVEL || - 1
+];
+
 let RequireUnCached = (module) => {
     delete require.cache[require.resolve(module)];
     return require(module);
@@ -8,13 +16,7 @@ let RequireUnCached = (module) => {
 
 describe('nodes.get', () => {
     it('ALL_IN_ONE_200_OK', async () => {
-        const pveapi = RequireUnCached('../../lib/pve-api')(
-            process.env.UNAME,
-            process.env.PASSWORD,
-            process.env.REALM || 'pam',
-            process.env.HOST_NAME || process.env.HOST_IPV4,
-            process.env.VERBOSE_LEVEL || -1
-        );
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
 
         pveapi.SetSslVerifyHost(false);
         pveapi.SetSslVerifyPeer(false);
@@ -24,13 +26,7 @@ describe('nodes.get', () => {
     }).timeout(10000);
 
     it('STANDALONE_200_OK', async () => {
-        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(
-            process.env.UNAME,
-            process.env.PASSWORD,
-            process.env.REALM || 'pam',
-            process.env.HOST_NAME || process.env.HOST_IPV4,
-            process.env.VERBOSE_LEVEL || -1
-        );
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
 
         pve_nodes_api.SetSslVerifyHost(false);
         pve_nodes_api.SetSslVerifyPeer(false);
@@ -43,13 +39,7 @@ describe('nodes.get', () => {
 
 describe('nodes.node.get', () => {
     it('ALL_IN_ONE_200_OK', async () => {
-        const pveapi = RequireUnCached('../../lib/pve-api')(
-            process.env.UNAME,
-            process.env.PASSWORD,
-            process.env.REALM || 'pam',
-            process.env.HOST_NAME || process.env.HOST_IPV4,
-            process.env.VERBOSE_LEVEL || -1
-        );
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
 
         pveapi.SetSslVerifyHost(false);
         pveapi.SetSslVerifyPeer(false);
@@ -63,13 +53,7 @@ describe('nodes.node.get', () => {
     }).timeout(10000);
 
     it('STANDALONE_200_OK', async () => {
-        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(
-            process.env.UNAME,
-            process.env.PASSWORD,
-            process.env.REALM || 'pam',
-            process.env.HOST_NAME || process.env.HOST_IPV4,
-            process.env.VERBOSE_LEVEL || -1
-        );
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
 
         pve_nodes_api.SetSslVerifyHost(false);
         pve_nodes_api.SetSslVerifyPeer(false);
@@ -78,6 +62,156 @@ describe('nodes.node.get', () => {
         assert.notEqual(process.env.PROXMOX_NODE, "");
 
         var response = await pve_nodes_api.Node(process.env.PROXMOX_NODE).Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+});
+
+describe('nodes.node.apt.get', () => {
+    it('ALL_IN_ONE_200_OK', async () => {
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
+
+        pveapi.SetSslVerifyHost(false);
+        pveapi.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pveapi.Nodes.Node(process.env.PROXMOX_NODE).Apt.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+
+    it('STANDALONE_200_OK', async () => {
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
+
+        pve_nodes_api.SetSslVerifyHost(false);
+        pve_nodes_api.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pve_nodes_api.Node(process.env.PROXMOX_NODE).Apt.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+});
+
+describe('nodes.node.apt.changelog', () => {
+    it('ALL_IN_ONE_200_OK', async () => {
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
+
+        pveapi.SetSslVerifyHost(false);
+        pveapi.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pveapi.Nodes.Node(process.env.PROXMOX_NODE).Apt.ChangeLog.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+
+    it('STANDALONE_200_OK', async () => {
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
+
+        pve_nodes_api.SetSslVerifyHost(false);
+        pve_nodes_api.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pve_nodes_api.Node(process.env.PROXMOX_NODE).Apt.ChangeLog.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+});
+
+describe('nodes.node.apt.repositories.get', () => {
+    it('ALL_IN_ONE_200_OK', async () => {
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
+
+        pveapi.SetSslVerifyHost(false);
+        pveapi.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pveapi.Nodes.Node(process.env.PROXMOX_NODE).Apt.Repositories.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+
+    it('STANDALONE_200_OK', async () => {
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
+
+        pve_nodes_api.SetSslVerifyHost(false);
+        pve_nodes_api.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pve_nodes_api.Node(process.env.PROXMOX_NODE).Apt.Repositories.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+});
+
+describe('nodes.node.apt.update.get', () => {
+    it('ALL_IN_ONE_200_OK', async () => {
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
+
+        pveapi.SetSslVerifyHost(false);
+        pveapi.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pveapi.Nodes.Node(process.env.PROXMOX_NODE).Apt.Update.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+
+    it('STANDALONE_200_OK', async () => {
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
+
+        pve_nodes_api.SetSslVerifyHost(false);
+        pve_nodes_api.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pve_nodes_api.Node(process.env.PROXMOX_NODE).Apt.Update.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+});
+
+describe('nodes.node.apt.version.get', () => {
+    it('ALL_IN_ONE_200_OK', async () => {
+        const pveapi = RequireUnCached('../../lib/pve-api')(...envTestParams);
+
+        pveapi.SetSslVerifyHost(false);
+        pveapi.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pveapi.Nodes.Node(process.env.PROXMOX_NODE).Apt.Version.Get();
+        assert.equal(response.statusCode, 200);
+
+    }).timeout(10000);
+
+    it('STANDALONE_200_OK', async () => {
+        const pve_nodes_api = RequireUnCached('../../lib/pve-nodes-api')(...envTestParams);
+
+        pve_nodes_api.SetSslVerifyHost(false);
+        pve_nodes_api.SetSslVerifyPeer(false);
+
+        assert.notEqual(process.env.PROXMOX_NODE, undefined);
+        assert.notEqual(process.env.PROXMOX_NODE, "");
+
+        var response = await pve_nodes_api.Node(process.env.PROXMOX_NODE).Apt.Version.Get();
         assert.equal(response.statusCode, 200);
 
     }).timeout(10000);
